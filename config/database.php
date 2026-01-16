c30d0b98401305288a208f8826594f0cb01cd3b2
Radja Aulia Al Ramdani <radjaauliaalramdani@gmail.com>
2026-01-16 19:51:20 +0700

    🔧 fix (database config) : enable encryption and trust server certificate settings for SQL Server connections

--- config/database.php
+++ config/database.php
@@ -109,8 +109,8 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+            'encrypt' => env('DB_ENCRYPT', 'yes'),
+            'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
         'sqlsrv_dn_member' => [
             'driver' => 'sqlsrv',
@@ -123,6 +123,8 @@ return [
             'prefix' => '',
             'encrypt' => false,
             'trust_server_certificate' => true,
+            'encrypt' => env('DB_ENCRYPT', 'yes'),
+            'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
 
